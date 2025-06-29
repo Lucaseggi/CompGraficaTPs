e2202f000ce2fdcbfa7cf00087244b3bf0e6640f
Luca Seggiaro <lseggiaro@itba.edu.ar>
2025-06-28 21:03:39 -0300

    feat: import textures, and impl warehouse ones

--- TP/warehouse.js
+++ TP/warehouse.js
@@ -1,8 +1,36 @@
 import * as THREE from 'three';
-import { buildGrid } from './grid';
 
-const colors = [0xf5f5f5, 0xf5f5f5, 0x2e2e2e];
 const WarehouseMaterial = THREE.MeshStandardMaterial;
+
+const textureFolder = '/textureMaps'
+const materialBaseFile = '/CorrugatedMetalPanel02_1K_BaseColor.png'
+const materialNormalFile = '/CorrugatedMetalPanel02_1K_Normal.png'
+
+const materialMap = new THREE.TextureLoader().load(textureFolder + materialBaseFile, texture => {
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(4, 1);
+});
+const materialNormal = new THREE.TextureLoader().load(textureFolder + materialNormalFile);
+
+const floorMaterialBaseFile = '/StoneTilesFloor01_1K_BaseColor.png'
+const floorMaterialNormalFile = '/StoneTilesFloor01_1K_Normal.png'
+
+const floorRepeatX = 9;
+const floorRepeatY = floorRepeatX / 3 * 2;
+
+
+const floorMaterialMap = new THREE.TextureLoader().load(textureFolder + floorMaterialBaseFile, texture => {
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(floorRepeatX, floorRepeatY);
+});
+const floorMaterialNormal = new THREE.TextureLoader().load(textureFolder + floorMaterialNormalFile, texture => {
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(floorRepeatX, floorRepeatY);
+});
+
 
 class Warehouse {
     constructor() {
@@ -17,14 +45,14 @@ class Warehouse {
         const warehouse = new THREE.Group();
 
         const cylinderGeometry = new THREE.CylinderGeometry(this.depth / 2, this.depth / 2, this.width, 4, 1, false, 0, Math.PI);
-        const cylinderMaterial = new WarehouseMaterial({ color: colors[0], side: THREE.BackSide });
+        const cylinderMaterial = new WarehouseMaterial({ side: THREE.BackSide, map: materialMap});
         const cylinder = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
         warehouse.add(cylinder);
 
         cylinder.position.y = this.height;
         cylinder.rotation.z = Math.PI / 2;
 
-        const wmaterial = new WarehouseMaterial({ color: colors[1], side: THREE.BackSide });
+        const wmaterial = new WarehouseMaterial({side: THREE.BackSide, map: materialMap});
         const geometries = [
             new THREE.PlaneGeometry(this.width, this.height),
             new THREE.PlaneGeometry(this.width, this.height),
@@ -41,27 +69,20 @@ class Warehouse {
 
         geometries.map((geom, i) => {
             const wall = new THREE.Mesh(geom, wmaterial);
-            const wallGrid = buildGrid(this.height, wallPos[i][4], 1);
             const wallGroup = new THREE.Group();
             wallGroup.add(wall)
-            wallGroup.add(wallGrid);
 
             wallGroup.position.set(wallPos[i][0], wallPos[i][1], wallPos[i][2]);
             wall.rotation.y = wallPos[i][3];
-            wallGrid.rotation.z = Math.PI / 2;
-            wallGrid.rotation.y = wallPos[(i + 2) % 4][3];
 
             warehouse.add(wallGroup)
         });
         
-        const fmaterial = new WarehouseMaterial({ color: colors[2], side: THREE.BackSide });
+        const fmaterial = new WarehouseMaterial({ side: THREE.BackSide, map: floorMaterialMap, normalMap: floorMaterialNormal });
         const fgeometry = new THREE.PlaneGeometry(this.width, this.depth);
         const floor = new THREE.Mesh(fgeometry, fmaterial);
         floor.rotation.x = Math.PI / 2;
         warehouse.add(floor);
-
-        const floorGrid = buildGrid(this.width, this.depth);
-        warehouse.add(floorGrid);
 
         return warehouse;
     }
